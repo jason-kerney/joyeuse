4eb5d4d7bc322f8cfd4bdefc279975fbec60268d
MobKerney <jason.kerney@hunterindustries.com>
2017-05-01 14:21:57 -0700

    thinking about shape.

--- notes/joyNotes.js
+++ notes/joyNotes.js
@@ -5,31 +5,34 @@ var signet = joy.signet;
 signet.subtype('string')('char', function (value) { return value.length === 1 });
 signet.subtype('string')('shortState', function (value) { return value.length === 2 && value === value.toUpperCase() });
 
-var baseSchema = 
+var baseSchema =
     {
-//////// Device to User Example ////////
+        //////// Device to User Example ////////
         SchemaName: null,
-        RestrictColumns: false,
-        MyDevice : Joy.table('MyDevice', {
+        DbQueryColumns: true,
+        DbQueryTables: true,
+        MyDevice: Joy.table('MyDevice', {
             tableName: 'device',
-            restrictColumns: true,
+            dbQueryColumns: false,
             key: ['id'],
             id: joy.type('int').readOnly(),
-            serialNumber: 
-                joy
+            serialNumber:
+            joy
                 .type('GUID')
                 .hidden()
                 .readonly()
                 .init(guid.new),
             active: 'boolean',
-            users: 'MyUserDevice.Users'}),
+            users: 'MyUserDevice.Users'
+        }),
 
         MyUser: Joy.table('MyUser', {
             tableName: 'user',
             key: ['ssoUserId'],
             ssoUserId: joy.readOnly('int'),
             name: 'string',
-            devices: 'MyUserDevice.Devices'}),
+            devices: 'MyUserDevice.Devices'
+        }),
 
         MyUserDevice: Joy.table('MyUserDevice', {
             tableName: 'userDevice',
@@ -44,8 +47,8 @@ var baseSchema =
             ],
         }),
 
-//////// Cartesian Join Example ////////
-        MyState: Joy.table('MyState',{
+        //////// Cartesian Join Example ////////
+        MyState: Joy.table('MyState', {
             tableName: 'state',
             key: ['id'],
             id: 'int',
@@ -58,7 +61,7 @@ var baseSchema =
             letter: 'char',
         }),
 
-        MyCartesian: Joy.table('MyCartesian',{
+        MyCartesian: Joy.table('MyCartesian', {
             state: 'MyState.state',
             letter: 'MyLetters.letter',
             relations: [
@@ -101,51 +104,56 @@ var db = require("joyeuse").HandleReturnChecks(model => {
 var deviceTable = db.MyDevice;
 var userTable = db.MyUser;
 
-var devices = 
+var devices =
     deviceTable
-    .include(deviceTable.users)
-    .select(
+        .include(deviceTable.users) ``
+        .select(
         db.or(
             userTable.id.equals(5),
             deviceTable.id.equals(13),
             db.equals('serialNumber', '120938')
         ).and
-        (
+            (
             deviceTable.active
-        )
-    );
+            )
+        );
 
-var users = 
+var users =
     userTable.select(db.all());
 
-var userNames = 
+db.returnQueies(function () {
+    var usersQuery =
+        userTable.select(db.all());
+});
+
+var userNames =
     userTable.select(db.all(), userTable.name);
 
-var usersForDevice = 
+var usersForDevice =
     userTable
-    .select(db.MyDevice.id.equals(42));
+        .select(db.MyDevice.id.equals(42));
 
-var pagedUsersForDevice = 
+var pagedUsersForDevice =
     userTable
-    .select(
-        db.MyDevice.id.equals(42), 
-        db.page({size: 5, pageNumber: 1})
-    );
+        .select(
+        db.MyDevice.id.equals(42),
+        db.page({ size: 5, pageNumber: 1 })
+        );
 
-var success = 
+var success =
     deviceTable
-    .delete(devices.first());
+        .delete(devices.first());
 
 var success =
     deviceTable.delete(db.all());
 
 // fails wont work
-var success = 
+var success =
     deviceTable
-    .delete();
+        .delete();
 
 // fails wont work
-var users = 
+var users =
     userTable.select();
 
 
@@ -178,22 +186,21 @@ var users =
 
 ///-------------------------------------------
 
-var def = 
-  system.handleTypes
+var def =
+    system.handleTypes
         .cond('columnDef', (column) => { return column; })
         .cond('string', (typeString) => { return joy.type(typeString); })
         .match(input); // throws exception if input is not string or columnDef type
 
 var columns = [];
-var filters =[];
+var filters = [];
 var pagination = null;
 
 function setPageConfig(page) {
-    if (pagination === null){
+    if (pagination === null) {
         pagination = page;
     }
-    else
-    {
+    else {
         throw "cannot have more then one pagination configuration";
     }
 };
@@ -202,15 +209,15 @@ function ignore(_) {
     // intentionally left blank
 };
 
-var handler = 
-  system.handleTypes
+var handler =
+    system.handleTypes
         .cond('columnSelector', (column) => { columns.push(column); })
         .cond('filter', (filter) => { filters.push(filter); })
         .cond('pagination', setPageConfig)
         .cond('*', ignore)
         .match;
 
-parameters.forEach(function(element) {
+parameters.forEach(function (element) {
     handler(element);
 }, this);
 
@@ -241,7 +248,7 @@ if (filters.length === 0) {
 
 // jfp need this functionality.
 j.when(map) // defaults to indentity
- .cond(value1, handle_Input_When_Map_Returns_Value1) 
- .cond(value2, handle_Input_When_Map_Returns_Value2)
- .cond(value3, handle_Input_When_Map_Returns_Value3)
- .match(input);
+    .cond(value1, handle_Input_When_Map_Returns_Value1)
+    .cond(value2, handle_Input_When_Map_Returns_Value2)
+    .cond(value3, handle_Input_When_Map_Returns_Value3)
+    .match(input);
