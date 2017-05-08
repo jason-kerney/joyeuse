f51292e3307c5861bf36758d849e375b41d9e324
MobKerney <jason.kerney@hunterindustries.com>
2017-05-08 08:46:16 -0700

    refactored test namespaces

--- tests/test-utils/BeyondCompare4Reporter.js
+++ tests/test-utils/BeyondCompare4Reporter.js
@@ -9,9 +9,34 @@ function statPath(filePath) {
     }
 }
 
+
+
 function BeyondCompare4 () {
     return {
         name: 'BeyondCompare4',
+        canReportOn: function () {
+            return true;
+        },
+        report: function (approvedFilePath, receivedFilePath) {
+            var executionPath = '/Program Files/Beyond Compare 4/BCompare.exe';
+            var approvedPath = './' + approvedFilePath;
+            var receivedPath = './' + receivedFilePath;
+
+            var approvedStat = statPath(approvedPath);
+
+            if (!approvedStat) {
+                fs.writeFileSync(approvedPath, '');
+            }
+
+            spawn(executionPath, [receivedPath, approvedPath], { detached: true });
+
+        }
+    };
+};
+
+function BeyondCompare3 () {
+    return {
+        name: 'BeyondCompare3',
         canReportOn: function () {
             return true;
         },
--- tests/typesBuilder.tests.js
+++ tests/typesBuilder.tests.js
@@ -11,57 +11,55 @@ function approveIt(context, value) {
 }
 
 describe('type builder', function () {
-    describe('asVariant', function () {
-        it('should return for a hard coded variant type', function () {
-            this.verify(typeBuilder.asVariant('int; string'));
-        });
-
-        it('should return for an array of types', function () {
-            this.verify(typeBuilder.asVariant('int', 'boolean', 'string'));
-        });
-
-        it('should return a bounded int with a min of 0 and a max of 100', function () {
-            this.verify(typeBuilder.asBoundedInt(0, 100));
-        });
-
-        it('should return a bounded int with a min of 0 and a max of infinity', function () {
-            this.verify(typeBuilder.asBoundedInt(0));
-        });
-
-        it('should be able to classify undifinded as undefined', function () {
-            assert.isTrue(typeBuilder.isUndefined(undefined));
-        });
-
-        it('should determine a string is not undefined', function () {
-            assert.isFalse(typeBuilder.isUndefined(""));
-        });
-
-        it('should be able to create an untyped array type', function () {
-            this.verify(typeBuilder.asArray());
-        })
-
-        it('should be able to create a type array type', function () {
-            this.verify(typeBuilder.asArray('int'));
-        });
-
-        it('should be able to create a formatted string', function () {
-            this.verify(typeBuilder.asFormattedString('.*'));
-        });
-
-        it('should be able to create an optional parameter', function () {
-            this.verify(typeBuilder.asOptionalParameter('boolean'));
-        });
-
-        it('should be able to create an optional property as a veriant of undefined', function () {
-            this.verify(typeBuilder.asOptionalProperty('string'));
-        });
-
-        it('should be able to create method types', function () {
-            this.verify(typeBuilder.asMethod('string', 'int'));
-        });
-
-        it('should be able to create a method with multiple input parameters', function () {
-            this.verify(typeBuilder.asMethod(['int', 'boolean', 'string'], '*'))
-        })
+    it('should return for a hard coded variant type', function () {
+        this.verify(typeBuilder.asVariant('int; string'));
     });
+
+    it('should return for an array of types', function () {
+        this.verify(typeBuilder.asVariant('int', 'boolean', 'string'));
+    });
+
+    it('should return a bounded int with a min of 0 and a max of 100', function () {
+        this.verify(typeBuilder.asBoundedInt(0, 100));
+    });
+
+    it('should return a bounded int with a min of 0 and a max of infinity', function () {
+        this.verify(typeBuilder.asBoundedInt(0));
+    });
+
+    it('should be able to classify undifinded as undefined', function () {
+        assert.isTrue(typeBuilder.isUndefined(undefined));
+    });
+
+    it('should determine a string is not undefined', function () {
+        assert.isFalse(typeBuilder.isUndefined(""));
+    });
+
+    it('should be able to create an untyped array type', function () {
+        this.verify(typeBuilder.asArray());
+    })
+
+    it('should be able to create a type array type', function () {
+        this.verify(typeBuilder.asArray('int'));
+    });
+
+    it('should be able to create a formatted string', function () {
+        this.verify(typeBuilder.asFormattedString('.*'));
+    });
+
+    it('should be able to create an optional parameter', function () {
+        this.verify(typeBuilder.asOptionalParameter('boolean'));
+    });
+
+    it('should be able to create an optional property as a veriant of undefined', function () {
+        this.verify(typeBuilder.asOptionalProperty('string'));
+    });
+
+    it('should be able to create method types', function () {
+        this.verify(typeBuilder.asMethod('string', 'int'));
+    });
+
+    it('should be able to create a method with multiple input parameters', function () {
+        this.verify(typeBuilder.asMethod(['int', 'boolean', 'string'], '*'))
+    })
 });
