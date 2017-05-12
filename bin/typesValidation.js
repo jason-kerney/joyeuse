d7e88fadddb18908561d48657883ec755c136493
MobKerney <jason.kerney@hunterindustries.com>
2017-05-12 11:56:10 -0700

    added validation of objects and sub objects

--- bin/typesValidation.js
+++ bin/typesValidation.js
@@ -2,6 +2,41 @@
 var typeBuilder = require('./typeBuilder');
 var signet = typeBuilder.signet;
 
+function getBasicErrors(propertyName, typeInfo, value, errors) {
+    var isType = signet.isTypeOf(typeInfo)(value);
+    if (isType) {
+        return errors;
+    } else {
+        var newErrors = errors.slice();
+        newErrors.push(
+            {
+                property_name: propertyName,
+                type: typeInfo,
+                value_given: String(value),
+            }
+        );
+        return newErrors;
+    }
+}
+
+function getObjectErrors(propertyName, typeInfo, value, errors) {
+    var newErrors = errors.slice();
+    (Object.keys(typeInfo)).forEach(function(key) {
+        var subName = propertyName + '.' + key;
+        var subType = typeInfo[key];
+        var subValue = value[key];
+
+        var subErrors = getErrors(subName, subType, subValue);
+
+        for (var index = 0; index < subErrors.length; index++) {
+            var error = subErrors[index];
+            newErrors.push(error);
+        }
+    }, this);
+
+    return newErrors;
+}
+
 function getErrors() {
     var hasAllArguments = arguments.length === 3;
 
@@ -9,21 +44,14 @@ function getErrors() {
     var valueIndex = hasAllArguments ? 2 : 1;
 
     var propertyName = hasAllArguments ? arguments[0] : '';
-    var typeName = arguments[typeNameIndex];
-    var value = arguments[valueIndex]
-    ;
-    var isType = signet.isTypeOf(typeName)(value);
-    if (isType) {
-        return [];
-    } else {
-        return [
-            {
-                property_name: propertyName,
-                type: typeName,
-                value_given: value,
-            }
-        ];
+    var typeInfo = arguments[typeNameIndex];
+    var value = arguments[valueIndex];
+
+    if (typeof typeInfo === 'object') {
+        return getObjectErrors(propertyName, typeInfo, value, [])
     }
+
+    return getBasicErrors(propertyName, typeInfo, value, []);
 }
 
 module.exports = {
--- tests/typesValidation.tests.js
+++ tests/typesValidation.tests.js
@@ -38,5 +38,48 @@ describe('type validation builder', function () {
             var errors = typesValidation.getErrors('boolean', 'not a boolean');
             this.verify(pretyJson(errors));
         });
+
+        it.only('should return no errors when a field of a propety does match its type', function () {
+            var errors = typesValidation.getErrors({ aProperty: 'string' }, { aProperty: 'thingy' });
+            assert.lengthOf(errors, 0);
+        });
+
+        it.only('should return errors when a field of a propety does not match its type', function () {
+            var errors = typesValidation.getErrors('testObject', { aProperty: 'boolean' }, { aProperty: 'thingy' });
+            this.verify(pretyJson(errors));
+        });
+
+        it.only('should return errors when multiple propeties do not match thier type', function () {
+            var errors = typesValidation.getErrors('testObject', { aProperty: 'boolean', bProperty: 'int' }, { aProperty: 'thingy', bProperty: true });
+            this.verify(pretyJson(errors));
+        });
+
+        it.only('should return errors when some propeties do not match thier type', function () {
+            var errors = typesValidation.getErrors('testObject', { goodProperty: 'int', aProperty: 'boolean', bProperty: 'string' }, { goodProperty: 5, aProperty: 'thingy', bProperty: true });
+            this.verify(pretyJson(errors));
+        });
+
+        it.only('should return errors for missing propeties', function () {
+            var errors = typesValidation.getErrors('testObject', { goodProperty: 'int', aProperty: 'boolean', bProperty: 'string' }, { goodProperty: 5, aProperty: true });
+            this.verify(pretyJson(errors));
+        });
+
+        it.only('should return errors for sub-objects', function () {
+            var typeDef = {
+                aProperty: 'boolean',
+                bProperty: {
+                    age: 'int'
+                }
+            };
+            var value = {
+                aProperty: true,
+                bProperty: {
+                    age: 'two'
+                }
+            };
+            
+            var errors = typesValidation.getErrors('testObject', typeDef, value);
+            this.verify(pretyJson(errors));
+        });
     });
 });
