5495995ae1a2de84064e0d4ae00d63a290d0e64e
MobKerney <jason.kerney@hunterindustries.com>
2017-05-19 21:43:01 -0700

    removed dead code

--- bin/types.js
+++ bin/types.js
@@ -1,24 +1,9 @@
 'use strict';
-
-const typeBuilder = require('./typeBuilder');
-const validator = require('./typesValidation');
-const when = require('./when')();
-const getTypeNames = require('./types/typeNames');
-const baseTypes = require('./types/base')();
 const ip4 = require('./types/ip4')();
-const knexBaseTypes = require('./types/knexBase')();
-const connectionParts = require('./types/connectionParts')();
-
+const baseTypes = require('./types/base')();
 const knex = require('./types/knex')();
-
-const connectionString = 'knexConnectionString';
-const connectionObject = 'knexConnectionObject';
-const connectionFileObject = 'knexConnectionFileObject';
-const connectionTypeDefs = typeBuilder.asVariantDefString(connectionObject, connectionFileObject, connectionString);
-const signet = typeBuilder.signet;
-const typeNames = getTypeNames();
-
 const joyeuseTypes = require('./types/joyeuse')();
+const getTypeNames = require('./types/typeNames');
 
 module.exports = {
     ip4: ip4,
